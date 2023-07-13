4c831d70ef07427cc126b96c628e843e4a682ae9
RazyMSR <139257210+RazyMSR@users.noreply.github.com>
2023-07-13 09:50:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title ('my parents new healthy diner')
 
 
-streamlit.title2 ('Breakfast Menu')
+streamlit.title ('Breakfast Menu')
 
 ('omega3 & blueberry omlet')
 ('kale, spinach')
